66da8046434df3acb3cd68690c3e7f11c3422eb9
tpitale <tpitale@gmail.com>
2010-06-01 22:38:51 -0700

    minor tweaks to custom var js; updates to profiles

--- ga/custom_var_page_and_session.js
+++ ga/custom_var_page_and_session.js
@@ -2,7 +2,7 @@
 _gaq.push(['_setCustomVar', 1, 'purchase', 'level-2', 3]);
 _gaq.push(['_trackPageview']);
 
-// what do user do the rest of this session, or after an upgrade
+// what do users do the rest of this session, or after an upgrade
 _gaq.push(['_setCustomVar', 1, 'purchase', 'level-2', 2]);
 _gaq.push(['_setCustomVar', 2, 'upgrade', '1', 2]); // use slot 2
 _gaq.push(['_trackPageview']);
--- garb/profiles.rb
+++ garb/profiles.rb
@@ -3,11 +3,13 @@ puts account.id
 puts account.name
 
 profile = account.profiles.first
+puts profile.web_property_id # UA-65432-1
 puts profile.table_id # internal id for GA
 puts profile.title
 puts profile.account_name # matches account.name
-puts profile.account_id #matches account.id
-puts profile.web_property_id # UA-65432-1
+puts profile.account_id # matches account.id
 
-# Find the first profile with UA number
-profile = Profile.first(‘UA-65432-1’)
+# Find the first profile with UA number, explicit session
+profile = Profile.first('UA-65432-1', session)
+# Find the first profile with this table id, global session
+profile = Profile.first('1234567')
