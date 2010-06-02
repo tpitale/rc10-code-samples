9a28f1d49296bd9d7ba63a209058aad570aced00
tpitale <tpitale@gmail.com>
2010-06-01 21:08:01 -0700

    fixed whitespace

--- ga/require_ga.js
+++ ga/require_ga.js
@@ -1,12 +1,12 @@
 (function() {
-  var ga = document.createElement('script');
-  var src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www');
+  var ga = document.createElement('script');
+  var src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www');
   src += '.google-analytics.com/ga.js';
 
   ga.type = 'text/javascript';
   ga.async = true;
   ga.src = src;
 
-  var s = document.getElementsByTagName('script')[0];
+  var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(ga, s);
 })();
