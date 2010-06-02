e1115773f3cef65e84a554b7df8c4144f0cca33f
tpitale <tpitale@gmail.com>
2010-06-01 21:07:20 -0700

    broke lines down for font size increase in presentation

--- ga/require_ga.js
+++ ga/require_ga.js
@@ -1,5 +1,12 @@
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  var ga = document.createElement('script');
+  var src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www');
+  src += '.google-analytics.com/ga.js';
+
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = src;
+
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
 })();
