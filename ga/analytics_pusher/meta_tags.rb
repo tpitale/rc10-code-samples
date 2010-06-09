ebebef62e89ec02ea61c1094387bd455b90e09e9
tpitale <tpitale@gmail.com>
2010-06-09 10:40:35 -0400

    update meta tag example

--- ga/analytics_pusher/meta_tags.rb
+++ ga/analytics_pusher/meta_tags.rb
@@ -1,6 +1,10 @@
 def analytics_pusher_meta_tag
   meta_tags = ""
-  meta_tags << %(<meta name="track-pageview" content="#{Rack::Utils.escape_html(@_pageview_to_track)}" />) if track_pageview?
-  meta_tags << %(<meta name="track-event" content="#{Rack::Utils.escape_html(@_event_to_track)}" />) if track_event?
+  
+  if track_pageview?
+    content = Rack::Utils.escape_html(@_pageview_to_track)
+    meta_tags << %(<meta name="track-pageview" content="#{content}" />)
+  end
+
   meta_tags.html_safe
 end
